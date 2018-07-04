8302dd862a64053e4aa0ae21e92356861fdf1ddf
root <1065792154@qq.com>
2018-07-04 21:59:07 +0800

    add jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,15 @@
 <?php
 
+
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Auth;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
@@ -31,7 +33,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+      'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+      'weixin_openid', 'weixin_unionid'
     ];
 
     /**
@@ -88,4 +91,17 @@ class User extends Authenticatable
 
         $this->attributes['avatar'] = $path;
     }
+
+
+          // Rest omitted for brevity
+
+      public function getJWTIdentifier()
+      {
+          return $this->getKey();
+      }
+
+      public function getJWTCustomClaims()
+      {
+          return [];
+      }
 }

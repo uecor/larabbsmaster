a1a3c8ae44ac5af684788eef57e27ce1b06f8f9a
root <1065792154@qq.com>
2018-07-05 22:25:32 +0800

    categories index

--- routes/api.php
+++ routes/api.php
@@ -54,7 +54,9 @@ $api->version('v1', [
           'limit' => config('api.rate_limits.access.limit'),
           'expires' => config('api.rate_limits.access.expires'),
       ], function ($api) {
-          // 游客可以访问的接口
+        // 游客可以访问的接口
+        $api->get('categories', 'CategoriesController@index')
+            ->name('api.categories.index');
 
           // 需要 token 验证的接口
           $api->group(['middleware' => 'api.auth'], function($api) {

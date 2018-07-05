b82619845d2d992ef92a192569a5454743cc6a5b
root <1065792154@qq.com>
2018-07-06 07:10:47 +0800

    topic update api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,5 +37,8 @@ class AppServiceProvider extends ServiceProvider
         \API::error(function (\Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
             abort(404);
         });
+        \API::error(function (\Illuminate\Auth\Access\AuthorizationException $exception) {
+          abort(403, $exception->getMessage());
+       });
     }
 }

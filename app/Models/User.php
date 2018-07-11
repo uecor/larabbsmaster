8a53638166cb510323d3b94fa7880c5487248898
root <1065792154@qq.com>
2018-07-11 14:15:39 +0800

    weapp users store

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,10 +32,12 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array
      */
-    protected $fillable = [
-      'name', 'phone', 'email', 'password', 'introduction', 'avatar',
-      'weixin_openid', 'weixin_unionid'
-    ];
+     protected $fillable = [
+             'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+             'weixin_openid', 'weixin_unionid', 'registration_id',
+             'weixin_session_key', 'weapp_openid',
+         ];
+
 
     /**
      * The attributes that should be hidden for arrays.
